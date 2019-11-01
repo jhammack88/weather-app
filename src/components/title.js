52ad8e6706a9b87b7d4c478c8a72bf7720567b59
jhammack88 <johnhammack88@gmail.com>
2019-11-01 16:04:28 -0600

    testing app.js file

--- src/components/title.js
+++ src/components/title.js
@@ -4,7 +4,7 @@ class Title extends React.Component {
     render() {
         return (
             <div>
-                <h1 className="title-container_header">United Weather</h1>
+                <h1 className="title-container-header">United Weather</h1>
             </div>
         )
     }
