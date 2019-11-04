bfa2814b0f467571814820812c0dbd730346b65c
jhammack88 <johnhammack88@gmail.com>
2019-11-03 19:35:14 -0700

    working with styles

--- src/components/form.js
+++ src/components/form.js
@@ -4,9 +4,11 @@ class Form extends React.Component {
     render() {
         return (
             <form className="form" onSubmit={this.props.getWeather}>
-                <input className="inputs" type="text" name="city" placeholder="CITY..."/>
-                <input className="inputs" type="text" name="country" placeholder="COUNTRY..."/>
+                <input className="inputs" type="text" name="city" placeholder="Enter your city..."/>
+                <input className="inputs" type="text" name="country" placeholder="Enter your country..."/>
+                <div className="submit">
                 <button className="btn">Submit</button>
+                </div>
             </form>
         )
     }
